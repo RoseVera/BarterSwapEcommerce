c4417c4e28094f498e8715b2440f9d0b96e95774
Gülvera Yazılıtaş <98089865+RoseVera@users.noreply.github.com>
2025-05-30 16:22:50 +0300

    Delete

--- server/sequelize.js
+++
@@ -1,9 +0,0 @@
-// sequelize.js
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("E_commerce_webapp", "postgres", "Tulin8228", {
-  host: "localhost",
-  dialect: "postgres",
-}); 
-
-module.exports = sequelize;
